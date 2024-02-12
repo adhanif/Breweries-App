c12fcbb90cd8ad71f594ca13723be399c2954f5e
ad.hanif <ad.hanif@outlook.com>
2024-02-12 15:40:01 +0200

    pagination logic implemented in breweries

--- src/component/Breweries/Breweries.tsx
+++ src/component/Breweries/Breweries.tsx
@@ -1,18 +1,19 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import { useFetch } from '../../hook/useFetch';
 import { BreweryType } from '../../misc/types';
 import Brewery from '../Brewery/Brewery';
-import { Container, Grid, Typography } from '@mui/material';
+import { Container, Grid, Typography, Pagination, Box } from '@mui/material';
 import Loading from '../loading/Loading';
 
 export default function Breweries() {
   const url = 'https://api.openbrewerydb.org/v1/breweries';
   //   const url = 'https://fakestoreapi.com/users';
+  const itemsPerPage = 9;
+  const [currentPage, setCurrentPage] = useState(1);
 
-  const { data, loading, error } = useFetch<BreweryType>(url);
+  const { data, loading, error } = useFetch<BreweryType>(`${url}`);
 
   const memoizedBreweries = useMemo(() => {
-    console.log('Recalculating memoizedBreweries');
     return data || [];
   }, [data]);
 
@@ -24,6 +25,16 @@ export default function Breweries() {
     return <div>Error: {error}</div>;
   }
 
+  const handlePageChange = (
+    event: React.ChangeEvent<unknown>,
+    page: number,
+  ) => {
+    setCurrentPage(page);
+  };
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const displayedBreweries = memoizedBreweries.slice(startIndex, endIndex);
   return (
     <Container>
       <Typography
@@ -38,9 +49,9 @@ export default function Breweries() {
       >
         AFFILIATED BREWERIES
       </Typography>
-      <Grid container columnSpacing={5} rowSpacing={5} marginBottom={30}>
-        {memoizedBreweries &&
-          memoizedBreweries.map((brewery) => {
+      <Grid container columnSpacing={5} rowSpacing={5} marginBottom={10}>
+        {displayedBreweries &&
+          displayedBreweries.map((brewery) => {
             return (
               <Grid item key={brewery.id} xs={12} sm={6} md={4} lg={4}>
                 <Brewery brewery={brewery} />
@@ -48,6 +59,13 @@ export default function Breweries() {
             );
           })}
       </Grid>
+      <Box sx={{ display: 'flex', justifyContent: 'center' }} marginBottom={30}>
+        <Pagination
+          count={Math.ceil(memoizedBreweries.length / itemsPerPage)}
+          page={currentPage}
+          onChange={handlePageChange}
+        />
+      </Box>
     </Container>
   );
 }
